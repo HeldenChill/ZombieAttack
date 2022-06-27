e474b9c4db672bde000e53ed1f739d406c8a0d6d
HeldenChill <71561065+HeldenChill@users.noreply.github.com>
2022-06-27 14:44:10 +0700

    27/6/2022 - 2

--- Assets/ZombieAttackGame/Scripts/Core/Gun.cs
+++ Assets/ZombieAttackGame/Scripts/Core/Gun.cs
@@ -8,19 +8,13 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public float rateOfFire = 2;
-    public Camera fpsCamera;
     [SerializeField]
     GameObject bullet;
-    [SerializeField]
-    Transform shootPoint;
-    [SerializeField]
-    Transform aimPoint;
+    public Transform shootPoint;
     float counterTimeFire;
     void Start()
     {
         PrefabManager.Inst.CreatePool(bullet);
-        transform.LookAt(aimPoint.position);
-
         counterTimeFire = 1 / rateOfFire;
     }
 
@@ -29,19 +23,7 @@ public class Gun : MonoBehaviour
     {
         counterTimeFire -= Time.deltaTime;
 
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
-        {
-            if((shootPoint.position - hit.point).magnitude > 2)
-            {
-                transform.LookAt(hit.point);
-            }
-            
-        }
-        else
-        {
-            transform.LookAt(aimPoint.position);
-        }
+        
 
         if (Input.GetButton("Fire1") && counterTimeFire <= 0)
         {
--- Assets/ZombieAttackGame/Scripts/Core/MouseLook.cs
+++ Assets/ZombieAttackGame/Scripts/Core/MouseLook.cs
@@ -5,25 +5,51 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    bool isFPS = true;
     public Transform playerBody;
+    public Transform gun;
+    [SerializeField]
+    Transform aimPoint;
+    [SerializeField]
+    Camera playerCamera;
+    Gun gunScript;
     // Start is called before the first frame update
     float xRotation = 0f;
     void Start()
     {
-        
+        gun.transform.LookAt(aimPoint.position);
+        gunScript = gun.gameObject.GetComponent<Gun>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (isFPS)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, gunScript.range))
+            {
+                if ((gunScript.shootPoint.position - hit.point).magnitude > 1)
+                {
+                    gun.transform.LookAt(hit.point);
+                }
+                
+            }
+            else
+            {
+                gun.transform.LookAt(aimPoint.position);                
+            }
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation , -90f, 90f);
 
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            //gun.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        }
+            
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
